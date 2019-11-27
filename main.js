a8a69abae77cdd102ff6bfaeabe7ae6b452ce2aa
Sung Paik <sung.paik@codeimmersives.com>
2019-11-27 12:38:18 -0500

    completed test

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const Stack = function() {
+  return {
+    items: [],
+    add: function(str) {
+      this.items.push(str)
+    },
+    remove: function() {
+      return this.items.pop()
+    },
+    peek: function() {
+      return this.items[this.items.length - 1]
+    }
+  }
 }
 
 const Queue = function() {
+  return {
+    items: [],
+    add: function(str) {
+      this.items.push(str)
+    },
+    remove: function() {
+      return this.items.shift()
+    },
+    peek: function() {
+      return this.items[0]
+    }
+  }
 }
 
 module.exports = {
